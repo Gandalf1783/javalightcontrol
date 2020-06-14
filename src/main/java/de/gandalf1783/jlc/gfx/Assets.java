611ee95a809582b3e508ef5029934ab68ff4ed39
gandalf1783 <gandalf1783@gmail.com>
2020-06-14 19:25:45 +0200

    Assets File Update

--- src/main/java/de/gandalf1783/jlc/gfx/Assets.java
+++ src/main/java/de/gandalf1783/jlc/gfx/Assets.java
@@ -22,11 +22,8 @@ public class Assets {
 
 	public static void init() {
 
-
 		//font28 = FontLoader.loadFont("res/fonts/slkscr.ttf", 28);
 
-
-		SpriteSheet sheet = new SpriteSheet(ImageLoader.loadImage("textures/sheet.png"));
 		SpriteSheet horizontalScrollbarSheet = new SpriteSheet(ImageLoader.loadImage("ui/horizontial_scrollbar.png"));
 		SpriteSheet verticalScrollbarSheet = new SpriteSheet(ImageLoader.loadImage("ui/vertical_scrollbar.png"));
 
@@ -57,47 +54,6 @@ public class Assets {
 		btn_hover = ImageLoader.loadImage("ui/square_button_hover.png");
 		btn_press = ImageLoader.loadImage("ui/square_button_mouse_down.png");
 
-		inventoryScreen = ImageLoader.loadImage("textures/inventoryScreen.png");
-
-		wood = sheet.crop(width, height, width, height);
-
-		btn_start = new BufferedImage[2];
-		btn_start[0] = sheet.crop(width * 6, height * 4, width * 2, height);
-		btn_start[1] = sheet.crop(width * 6, height * 5, width * 2, height);
-
-		player_down = new BufferedImage[2];
-		player_up = new BufferedImage[2];
-		player_left = new BufferedImage[2];
-		player_right = new BufferedImage[2];
-
-		player_down[0] = sheet.crop(width * 4, 0, width, height);
-		player_down[1] = sheet.crop(width * 5, 0, width, height);
-		player_up[0] = sheet.crop(width * 6, 0, width, height);
-		player_up[1] = sheet.crop(width * 7, 0, width, height);
-		player_right[0] = sheet.crop(width * 4, height, width, height);
-		player_right[1] = sheet.crop(width * 5, height, width, height);
-		player_left[0] = sheet.crop(width * 6, height, width, height);
-		player_left[1] = sheet.crop(width * 7, height, width, height);
-
-		zombie_down = new BufferedImage[2];
-		zombie_up = new BufferedImage[2];
-		zombie_left = new BufferedImage[2];
-		zombie_right = new BufferedImage[2];
-
-		zombie_down[0] = sheet.crop(width * 4, height * 2, width, height);
-		zombie_down[1] = sheet.crop(width * 5, height * 2, width, height);
-		zombie_up[0] = sheet.crop(width * 6, height * 2, width, height);
-		zombie_up[1] = sheet.crop(width * 7, height * 2, width, height);
-		zombie_right[0] = sheet.crop(width * 4, height * 3, width, height);
-		zombie_right[1] = sheet.crop(width * 5, height * 3, width, height);
-		zombie_left[0] = sheet.crop(width * 6, height * 3, width, height);
-		zombie_left[1] = sheet.crop(width * 7, height * 3, width, height);
-
-		dirt = sheet.crop(width, 0, width, height);
-		grass = sheet.crop(width * 2, 0, width, height);
-		stone = sheet.crop(width * 3, 0, width, height);
-		tree = sheet.crop(0, 0, width, height * 2);
-		rock = sheet.crop(0, height * 2, width, height);
 	}
 
 }
